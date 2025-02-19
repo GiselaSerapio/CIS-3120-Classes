d249429bb77258f8851e70af8d3c17f96dbb0c4d
GuestAccount <guestaccount@snagtop.local>
2025-02-18 20:34:53 -0500

    completed

--- Animal.py
+++ Animal.py
@@ -1,7 +1,24 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name): 
+        self.__name = name
+        print("hello, I am",self.__name)
 
     def talk(self):
         print("hi")
+    def eat(self):
+        print(f"{self.__name} is eating.")
 
+    def sleep(self):
+        print(f"{self.__name} is sleeping.")
+
+    def move(self):
+        print(f"{self.__name} is moving.")
+
+    def describe(self):
+        print(f"{self.__name} is a wonderful animal.")
+
+    def introduce(self):
+        print(f"Hello, my name is {self.__name} and I am an animal.")
+    
+
+    
